5917e662d8cb59362bc4c6d0e2742bb279c7d5f3
Michelle Ranly <meranly8@gmail.com>
2021-04-06 23:01:55 -0500

    commit tree 3715d8ee5349efa1e5da1cdcaa631175502bf87b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(d => {
+        return d.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(d => {
+        return d.startsWith(name)
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter (d => {
+        return d.name === name
+    })
+}
